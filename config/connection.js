f254796a497c0a5a80cff8810231e6020d75e3f0
Dali Gala <habaresbit@gmail.com>
2019-04-24 19:48:19 -0400

    Whats up swaggy

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,18 @@
 const mysql = require("mysql");
 
 // Create connection
-const db = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "ro4otR01OOTrrOtt8",
-    database: "chatbots"
-});
+if (process.env.JAWSDB_URL) {
+    var db = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
+    const db = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "ro4otR01OOTrrOtt8",
+        database: "chatbots"
+    });
+
+};
 
 // Connect
 db.connect((err) => {
